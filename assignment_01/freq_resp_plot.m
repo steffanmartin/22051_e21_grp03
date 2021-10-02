45e011042357ab533c5820640e562349a72e3ae6
steffanmartin <s194006@student.dtu.dk>
2021-10-02 12:58:13 +0200

    new commit

---
+++ assignment_01/freq_resp_plot.m
@@ -0,0 +1,25 @@
+function freq_resp_plot(H,description)
+% Function to plot the frequency response of a transfer function H
+
+% Validate input parameter H
+if ~isa(H,'zpk') 
+    error('Not a zpk type!');
+end
+
+% Extract the numerator and denominator polynomials, a and b, from H
+[b,a] = tfdata(H,'v');
+
+% Frequency response of new filter transfer function
+[h,w] = freqz(b,a,'whole');
+
+figure('Name',description)
+title(description)
+plot(w/pi,20*log10(abs(h)))
+ax = gca;
+ax.XTick = 0:.1:2;
+ax.XLim = [0 2];
+xlabel('Normalized Frequency (\times\pi rad/sample)')
+ylabel('Magnitude (dB)')
+
+end
+
